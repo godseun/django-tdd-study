50d2fc5c118de4846dc0e91a420fb1642408f6c1
godseun <alcy1024@gmail.com>
2021-04-06 11:26:07 +0900

    pytest-django

--- blog/tests.py
+++ blog/tests.py
@@ -6,6 +6,24 @@ from blog.views import index
 
 
 # Create your tests here.
+class TestHomePage:
+    def test_root_url_resolves_to_blog_view(self):
+        found = resolve('/blog/')
+        assert found.func == index
+
+    def test_blog_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+
+        assert b"Django TDD." in response.content
+        assert response.content.endswith(b'</html>')
+
+
+class TestSmoke:
+    def test_bad_maths(self):
+        assert 1 + 1 == 2
+
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_blog_view(self):
         found = resolve('/blog/')
